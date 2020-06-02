88007b0a35d3716a3335a64fa03678721f062184
MainaGeorge <nuriszka@gmail.com>
2020-06-02 13:01:26 +0200

    Added the project set up

---
+++ DatingApp.API/Data/DatingAppContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingAppContext : DbContext
+    {
+        public DatingAppContext(DbContextOptions<DatingAppContext> options) : base(options) {}
+
+    }
+}
