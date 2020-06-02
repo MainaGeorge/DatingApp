a7cf650b796d6a66857051d5172d885b95058505
MainaGeorge <nuriszka@gmail.com>
2020-06-02 20:54:07 +0200

    Added IAuthentication repository interface

---
+++ DatingApp.API/Data/IAuthenticationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthenticationRepository
+    {
+        Task<UserModel> Register(UserModel user, string password);
+
+        Task<UserModel> Login(string username, string password);
+
+        Task<bool> UserExists(string username, string email);
+    }
+}
