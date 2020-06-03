72b3e9b29fd6bd7e788e210936ea1c4efaf3608e
MainaGeorge <nuriszka@gmail.com>
2020-06-03 16:41:20 +0200

    Added signing in using the user interface

--- DatingApp-SPA/src/app/_services/authentication-service.service.ts
+++ DatingApp-SPA/src/app/_services/authentication-service.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {LoginModel} from "../shared/models";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationServiceService {
+  private baseUrl = 'https://localhost:44337/api/'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  login(loginData: LoginModel){
+    return this.http.post<{token: string}>(this.baseUrl + 'authentication/login', loginData)
+      .pipe(map( response => {
+        if(response){
+          localStorage.setItem('token', response.token);
+        }
+      } ));
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(){
+    return !!localStorage.getItem('token') ;
+  }
 }
--- DatingApp-SPA/src/app/navigation-component/navigation-component.component.ts
+++ DatingApp-SPA/src/app/navigation-component/navigation-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginModel} from "../shared/models";
+import {AuthenticationServiceService} from "../_services/authentication-service.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-navigation-component',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation-component.component.css']
 })
 export class NavigationComponentComponent implements OnInit {
+  loggedInUser: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthenticationServiceService) { }
 
   ngOnInit(): void {
+    console.log(this.authService.isLoggedIn())
+    this.loggedInUser = this.authService.isLoggedIn();
   }
 
+  onSubmit(formElement: NgForm){
+    this.authService.login(formElement.value).subscribe( response => {
+        console.log('logged in')
+      this.loggedInUser = this.authService.isLoggedIn();
+      }, error => {
+      console.log(error.message);
+    })
+
+    formElement.reset();
+  }
+
+  onLogout(){
+    this.authService.logout();
+  }
 }
