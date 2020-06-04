ab4ed71081ae4df73f59ced0a1a6d9825b11e251
MainaGeorge <nuriszka@gmail.com>
2020-06-04 12:56:35 +0200

    Extended the usermodel class

--- DatingApp.API/Models/UserModel.cs
+++ DatingApp.API/Models/UserModel.cs
@@ -1,4 +1,10 @@
-﻿namespace DatingApp.API.Models
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.AspNetCore.Identity;
+
+namespace DatingApp.API.Models
 {
     public class UserModel
     {
@@ -7,5 +13,25 @@
         public string Email { get; set; }
         public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        public string Url { get; set; } 
     }
 }
